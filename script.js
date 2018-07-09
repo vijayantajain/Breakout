ec33494510ad4d4fdad44e6f1129cbfc7be73047
vijayantajain <vijayantashah@gmail.com>
2018-07-09 15:42:40 -0500

    Experiment with speed and direction of ball movement

--- script.js
+++ script.js
@@ -2,17 +2,21 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var x = canvas.width/2;
 var y = canvas.height-30;
-var dx = 2;
-var dy = -2;
+var dx = -0.5;
+var dy = -3.5;
 
-function draw(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+function drawBall(){
     ctx.beginPath();
     ctx.arc(x, y, 10, 0, Math.PI * 2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
+}
+
+function draw(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
     x += dx;
     y += dy;
 }
-setInterval(draw, 50);
+setInterval(draw, 10);
