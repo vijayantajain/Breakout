2cda2842a959fe37efd21e77dd12774b3a7ef1da
vijayantajain <vijayantashah@gmail.com>
2018-07-09 15:53:22 -0500

    Add conditions to bounce the ball off the walls

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var x = canvas.width/2;
 var y = canvas.height-30;
-var dx = -0.5;
-var dy = -3.5;
+var dx = 0.5;
+var dy = 0.5;
+var ballRadius = 10;
 
 function drawBall(){
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -16,7 +17,13 @@ function drawBall(){
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    if (y + dy < ballRadius || y + dy > canvas.height-ballRadius){
+        dy = (-1.08) * dy;
+    }
+    if (x + dx < ballRadius || x + dx > canvas.width-ballRadius){
+        dx = (-1.08)*dx;
+    }
     x += dx;
     y += dy;
 }
-setInterval(draw, 10);
+setInterval(draw, 5);
