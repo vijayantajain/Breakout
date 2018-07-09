1094cebe0bd34cd2c3baedcbb9d0497ebf25fda0
vijayantajain <vijayantashah@gmail.com>
2018-07-09 18:42:30 -0500

    Add paddle collision detection

--- script.js
+++ script.js
@@ -1,28 +1,52 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var x = canvas.width/2;
-var y = canvas.height-30;
-var dx = 0.5;
-var dy = 0.5;
+var y = canvas.height/2;
+var dx = 1;
+var dy = -1;
 var ballRadius = 10;
-var randomColor = "#0095DD";
 var paddleWidth = 75;
 var paddleHeight = 10;
 var paddleX = (canvas.width - paddleWidth) / 2;
+var rightPressed = false;
+var lefttPressed = false;
+var ballColor = "#0095DD";
+var paddleColor = "#ff7700";
 
-function getRandomColor(){
-    var rgb = "rgb(" + Math.floor(Math.random() * 256).toString(); 
-    for(let i = 0; i < 2; i++){
-        rgb += (',' + Math.floor(Math.random() * 256).toString() )
+document.addEventListener("keydown", keyDownHandler, false)
+document.addEventListener("keyup", keyUpHandler, false)
+
+/**
+ * This function handles the event when a key is pressed. It changes the
+ * variables 'rightPressed' and 'leftPressed' to true.
+ * @param {Event} e The event passed by the addEventListener function
+ */
+function keyDownHandler(e) {
+    if (e.keyCode == 39) {
+        rightPressed = true;
     }
-    rgb += ")";
-    return rgb;
+    else if (e.keyCode == 37) {
+        lefttPressed = true;
+    }
+}
+/**
+ * This function handles the event when the key is released after being pressed
+ * It releases
+ * @param {Event} e The event passed by the addEventListener function
+ */
+function keyUpHandler(e) {
+    if (e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if (e.keyCode == 37) {
+        lefttPressed = false;
+    }
 }
 
 function drawPaddle(){
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#ff7700";
+    ctx.fillStyle = paddleColor;
     ctx.fill();
     ctx.closePath();
 }
@@ -30,24 +54,43 @@ function drawPaddle(){
 function drawBall(){
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-    ctx.fillStyle = randomColor;
+    ctx.fillStyle = ballColor;
     ctx.fill();
     ctx.closePath();
 }
 
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     drawBall();
     drawPaddle();
-    if (y + dy < ballRadius || y + dy > canvas.height-ballRadius){
-        dy = (-1.08) * dy;
-        randomColor = getRandomColor();
-    }
+
     if (x + dx < ballRadius || x + dx > canvas.width-ballRadius){
-        dx = (-1.08)*dx;
-        randomColor = getRandomColor();
+        dx = -dx;
     }
+
+    if (y + dy < ballRadius ) {
+        dy = -dy;
+    } 
+    else if (y + dy > canvas.height - ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        }
+        else {
+            alert("GAME OVER");
+            document.location.reload();
+        }
+    }
+
+    
+    if (rightPressed && paddleX < canvas.width - paddleWidth) {
+        paddleX += 3;
+    } else if (lefttPressed && paddleX > 0) {
+        paddleX -= 3;
+    }
+
     x += dx;
     y += dy;
 }
+
 setInterval(draw, 5);
